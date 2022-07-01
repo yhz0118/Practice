e695333593c0f401ce27a7ae04685d39b7237715
Yunseo Choi <yunwest018@naver.com>
2022-07-01 09:04:01 +0900

    Create Composite.cs
    design pattern - composite
    used lang: c#

---
+++ C#/Composite.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Composite
+{
+    interface Graphic
+    {
+        public void Print();
+    }
+
+    class CompositeGraphic : Graphic
+    {
+        private List<Graphic> g = new List<Graphic>();
+
+        public void Print()
+        {
+            foreach (var m_child in g) { };
+        }
+
+        public void Add(Graphic shape)
+        {
+            g.Add(shape);
+        }
+
+        public void Remove(Graphic shape)
+        {
+            g.Remove(shape);
+        }
+     
+    }
+
+    class Ellipse: Graphic
+    {
+        private int m_id;
+
+        public Ellipse(int id)
+        {
+            this.m_id = id;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("ellipse: "+ m_id);
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var e1 = new Ellipse(10);
+            var e2 = new Ellipse(20);
+            var e3 = new Ellipse(30);
+
+            var cp1 = new CompositeGraphic();
+            var cp2 = new CompositeGraphic();
+            var all = new CompositeGraphic();
+
+            cp1.Add(e1); cp1.Add(e2); cp1.Add(e3);
+            cp2.Add(e1); cp2.Add(e2); cp2.Add(e3);
+
+            all.Add(cp1);
+            all.Add(cp2);
+            all.Print();
+        }
+    }
+
+
+}
