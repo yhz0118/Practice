5afc7c2f63ce86ca1ee62b75362254449a28d4ea
Yunseo Choi <59947514+yhz0118@users.noreply.github.com>
2022-07-14 13:12:00 +0900

    Add files via upload
    find minimum and maximum values
    used lang: c#

---
+++ C#/MinMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace MinMax
+{
+
+    class Program
+    {
+        public static int[] minmax(int min, int max)
+        {
+            int[] res = new int[2];
+
+            if (min > max)
+            {
+                res[0] = max;
+                res[1] = min;
+            }
+
+            else
+            {
+                res[0] = min;
+                res[1] = max;
+            }
+
+            return res;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            int[] result = Program.minmax(2, 7);
+
+            Console.WriteLine("Minimum value : {0}", result[0]);
+            Console.WriteLine("Maximum value : {0}", result[1]);
+        }
+    }
+}
