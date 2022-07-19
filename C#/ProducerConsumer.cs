9534df838c194931585d4a614a6f57ce2b40c396
Yunseo Choi <59947514+yhz0118@users.noreply.github.com>
2022-07-19 13:23:10 +0900

    Create ProducerConsumer.cs

---
+++ C#/ProducerConsumer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+class DataflowProducerConsumer
+{
+    static void Produce(ITargetBlock<byte[]> target)
+    {
+        var rand = new Random();
+
+        for (int i = 0; i < 100; ++ i)
+        {
+            var buffer = new byte[1024];
+            rand.NextBytes(buffer);
+            target.Post(buffer);
+        }
+
+        target.Complete();
+    }
+
+    static async Task<int> ConsumeAsync(ISourceBlock<byte[]> source)
+    {
+        int bytesProcessed = 0;
+
+        while (await source.OutputAvailableAsync())
+        {
+            byte[] data = await source.ReceiveAsync();
+            bytesProcessed += data.Length;
+        }
+
+        return bytesProcessed;
+    }
+
+    static async Task Main()
+    {
+        var buffer = new BufferBlock<byte[]>();
+        var consumerTask = ConsumeAsync(buffer);
+        Produce(buffer);
+
+        var bytesProcessed = await consumerTask;
+
+        Console.WriteLine($"Processed {bytesProcessed:#,#} bytes.");
+    }
+}
